c901156d9038e3884876e5b609ac0c7ff4d54aac
Meiram Shunshalin <meiram.shunshalin@nu.edu.kz>
2023-01-15 23:15:19 +0600

    Add home page

---
+++ src/main/java/com/naturalgoods/backend/welcome/Welcome.java
@@ -0,0 +1,16 @@
+package com.naturalgoods.backend.welcome;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class Welcome {
+
+    @GetMapping
+    public String defultPage() {
+        return "Natural Goods webserver is successfully deployed!";
+    }
+}
