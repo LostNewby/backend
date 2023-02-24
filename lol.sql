c244833e7c3ec05d918200227913f97e248d3dac
Meiram Shunshalin <meiram.shunshalin@nu.edu.kz>
2023-02-24 22:48:40 +0600

    categories

---
+++ lol.sql
@@ -0,0 +1,80 @@
+
+
+insert into
+  product(category_id, name_en, name_ru, name_kz)
+values
+    (2, 'Tomato', 'Помидор', 'Қызанақ'),
+    (2, 'Cucumber', 'Огурец', 'Қияр'),
+    (2, 'Potato', 'Картофель', 'Ақтүйнек'),
+    (2, 'Onion', 'Лук', 'Пияз'),
+    (2, 'Garlic', 'Чеснок', 'Сарымсақ'),
+    (2, 'Carrot', 'Морковь', 'Сәбіз'),
+    (2, 'Eggplant', 'Баклажан', 'Баялды'),
+    (2, 'Cabbage', 'Капуста', 'Орамжапырақ');
+
+
+insert into
+  product(category_id, name_en, name_ru, name_kz)
+values
+    (3, 'Kumis', 'Кумыс', 'Кумыс'),
+    (3, 'Airan', 'Айран', 'Айран'),
+    (3, 'Shubat', 'Шубат', 'Шубат'),
+    (3, 'Milk', 'Молоко', 'Молоко'),
+    (3, 'Saumal', 'Саумал', 'Саумал'),
+    (3, 'Kampot', 'Кампот', 'Кампот');
+
+insert into
+  product(category_id, name_en, name_ru, name_kz)
+values
+    (4, 'Horse meat', 'Конина', 'жылқы  еті'),
+    (4, 'Cow meat', 'Говядина', 'Сиыр еті'),
+    (4, 'Sheep meat', 'Баранина', 'Қой еті'),
+    (4, 'Chicken meat', 'Куриное мясо', 'Тауық еті'),
+    (4, 'Fish', 'Рыба', 'Балық'),
+    (4, 'Camel Meat', 'Верблюжье мясо', 'түйе еті');
+
+insert into
+  product(category_id, name_kz, name_ru, name_en)
+values
+  (1, 'Apple', 'Яблоко', 'Алма'),
+  (1, 'Banana', 'Банан', 'Банан'),
+  (1, 'Lemon', 'Лимон', 'Лимон'),
+  (1, 'Orange', 'Апельсин', 'Апельсин'),
+  (1, 'Berries', 'Ягоды', 'Жидектер'),
+  (1, 'Pear', 'Груша', 'Алмұрт'),
+  (1, 'Apricot', 'Абрикос', 'Өрік'),
+  (1, 'Peach', 'Персик', 'Шабдалы'),
+  (1, 'Plum', 'Слива', 'Алхоры');
+
+insert into
+  product(category_id, name_en, name_ru, name_kz)
+values
+    (2, 'Tomato', 'Помидор', 'Қызанақ'),
+    (2, 'Cucumber', 'Огурец', 'Қияр'),
+    (2, 'Potato', 'Картофель', 'Ақтүйнек'),
+    (2, 'Onion', 'Лук', 'Пияз'),
+    (2, 'Garlic', 'Чеснок', 'Сарымсақ'),
+    (2, 'Carrot', 'Морковь', 'Сәбіз'),
+    (2, 'Eggplant', 'Баклажан', 'Баялды'),
+    (2, 'Cabbage', 'Капуста', 'Орамжапырақ');
+
+
+insert into
+  product(category_id, name_en, name_ru, name_kz)
+values
+    (3, 'Kumis', 'Кумыс', 'Кумыс'),
+    (3, 'Airan', 'Айран', 'Айран'),
+    (3, 'Shubat', 'Шубат', 'Шубат'),
+    (3, 'Milk', 'Молоко', 'Молоко'),
+    (3, 'Saumal', 'Саумал', 'Саумал'),
+    (3, 'Kampot', 'Кампот', 'Кампот');
+
+insert into
+  product(category_id, name_en, name_ru, name_kz)
+values
+    (4, 'Horse meat', 'Конина', 'жылқы  еті'),
+    (4, 'Cow meat', 'Говядина', 'Сиыр еті'),
+    (4, 'Sheep meat', 'Баранина', 'Қой еті'),
+    (4, 'Chicken meat', 'Куриное мясо', 'Тауық еті'),
+    (4, 'Fish', 'Рыба', 'Балық'),
+    (4, 'Camel Meat', 'Верблюжье мясо', 'түйе еті');
