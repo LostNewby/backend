58c0703faadb727b1a61787534da3ac270b9d899
LostNewby <alisher.zhakenov.h@gmail.com>
2023-02-27 10:19:14 +0600

    filter addition

--- src/main/java/com/naturalgoods/backend/dto/ProductCardsDto.java
+++ src/main/java/com/naturalgoods/backend/dto/ProductCardsDto.java
@@ -2,7 +2,14 @@ package com.naturalgoods.backend.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class ProductCardsDto {
     Long id;
+    String name;
+    String description;
+    BigDecimal rating;
+    String photo=null;
+    Long price;
 }
--- src/main/java/com/naturalgoods/backend/record/RecordService.java
+++ src/main/java/com/naturalgoods/backend/record/RecordService.java
@@ -8,6 +8,7 @@ import org.springframework.util.CollectionUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -16,10 +17,10 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class RecordService {
     private final EntityManager entityManager;
-
+    //TODO p->pt
     public List<ProductCardsDto> filter(String name, List<Long> categoryId, List<Long> productId, List<Long> productTypeId, SortingType sortingType, Long minPrice, Long maxPrice, String region){
         StringBuilder builder = new StringBuilder();
-        builder.append("select r.id, r.region, r.price from records r left join product_type pt on pt.id = r.product_type_id left join product p on p.id = pt.product_id left join category c on c.id = p.category_id where r.region = :region and r.price >= :minPrice  ");
+        builder.append("select r.id, pt.name_ru, r.description, r.rating, r.price from records r left join product_type pt on pt.id = r.product_type_id left join product p on p.id = pt.product_id left join category c on c.id = p.category_id where r.region = :region and r.price >= :minPrice  ");
         if(!CollectionUtils.isEmpty(categoryId)){
             builder.append("and c.id in :category ");
         }
@@ -33,7 +34,7 @@ public class RecordService {
             builder.append("and r.price <= :maxPrice ");
         }
         if(Objects.nonNull(name) && !name.equals("")){
-            builder.append("and pt.name_kz like :name or pt.name_ru like :name or pt.name_en like :name ");
+            builder.append("and lower(pt.name_kz) like lower(:name) or lower(pt.name_ru) like lower(:name) or lower(pt.name_en) like lower(:name) ");
         }
         if(sortingType.equals(SortingType.PRICE)){
             builder.append("order by r.price");
@@ -64,6 +65,10 @@ public class RecordService {
             Object[] obj = (Object[]) o;
             ProductCardsDto productCardsDto = new ProductCardsDto();
             productCardsDto.setId(Long.valueOf(String.valueOf(obj[0])));
+            productCardsDto.setName(String.valueOf(obj[1]));
+            productCardsDto.setDescription(String.valueOf(obj[2]));
+            productCardsDto.setRating(BigDecimal.valueOf(Double.valueOf(String.valueOf(obj[3]))));
+            productCardsDto.setPrice(Long.valueOf(String.valueOf(obj[4])));
             return productCardsDto;
         }).collect(Collectors.toList());
 
